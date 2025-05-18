ee498becefb9d577373c71eda88b5c48485fd5e1
divyanshi-rasotia <ddivyanshi693@gmail.com>
2025-05-18 18:54:20 +0530

    One of the final state

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
 
 
-// const cards = document.querySelectorAll('.card');
-// const background = document.getElementById('background');
-// const movieInfo = document.getElementById('movieInfo');
-
-// cards.forEach(card => {
-//   card.addEventListener('mouseenter', () => {
-//     const bg = card.getAttribute('data-bg');
-//     const title = card.getAttribute('data-title');
-//     const description = card.getAttribute('data-description');
-
-//     background.style.backgroundImage = `url(${bg})`;
-//     movieInfo.innerHTML = `<h2>${title}</h2><p>${description}</p>`;
-//   });
-// });
-
 const cards = document.querySelectorAll('.card');
 const background = document.getElementById('background');
 const movieInfo = document.getElementById('movieInfo');
@@ -39,7 +24,7 @@ cards.forEach(card => {
         <p class="movie-description">${description}</p>
         <div class="movie-actions">
           <button class="stream-btn">STREAM NOW</button>
-          <button class="like-btn">♥</button>
+        <button class="like-btn">♥</button>
         </div>
       </div>
     `;
